37864f03f9d57ba0b5112696c9f90fc26b3671a6
minhtuann <lamtuanvt9x@gmail.com>
2023-04-12 22:51:45 +0300

    adjustable screen for mobile devices, added fun facts

--- main.js
+++ main.js
@@ -4,12 +4,23 @@ const messageContainer = document.getElementById("message-container");
 document.getElementById("time").style.display = "none";
 document.getElementById("place").style.display = "none";
 
+// fetch an interesting science fact from the API when the page loads with an element with id="science-fact"
+async function fetchScienceFact() {
+  const response = await fetch("https://uselessfacts.jsph.pl/random.json?language=en");
+  const data = await response.json();
+  document.getElementById("science-fact").innerHTML = "Once a upon a time, a wise man in Kaari said: "+ data.text;
+}
+// run the function when the page loads
+fetchScienceFact();
+
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
+  // hide the element with id = "science-fact"
+  document.getElementById("science-fact").style.display = "none";
   form.style.display = "none";
   const name = event.target.name.value;
   const actor = event.target.actor.value;
-  const language = event.target.language.value;
+  const language = event.target.language.value;    
   const message = {
     role: "user",
     content: `Craft a funny message to ${name} as a character of ${actor} (anything even fictional character) inviting ${name} to a get-together night because you have not seen them for a long time and they are missed. 
